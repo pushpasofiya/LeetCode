a4103d8c9718a55285dffedbca3d18b6383e6dc9
Pushpa Sofiya22 <pushpasofiyaixa@gmail.com>
2026-08-23 14:05:04 +0530

    Time: 0 ms (100%), Space: 77.1 MB (46.44%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -12,48 +12,47 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
 
-        ListNode* head = NULL;
-        ListNode* tail = NULL;
+        int sum=0;
+        int carry=0;
+        int digit=0;
+        ListNode* head=NULL;
+        ListNode* tail=NULL;
+        while(l1!=NULL || l2!=NULL)
+        {
+            sum=(l1?l1->val:0)+(l2?l2->val:0)+carry;
+            digit=sum%10;
+            carry=sum/10;
 
-        int carry = 0;
+            ListNode* new_node=new ListNode(digit);
 
-        while (l1 != NULL || l2 != NULL) {
-
-            int sum = (l1 ? l1->val : 0)
-                    + (l2 ? l2->val : 0)
-                    + carry;
-
-            int digit = sum % 10;
-            carry = sum / 10;
-
-            ListNode* new_node = new ListNode(digit);
-
-            
-            if (head == NULL) {
-                head = new_node;
-                tail = new_node;
+            if(head==NULL)
+            {
+                head=new_node;
+                tail=new_node;
             }
-            
-            else {
-                tail->next = new_node;
-                tail = new_node;
+            else
+            {
+                tail->next=new_node;
+                tail=new_node;
             }
+            if(l1!=NULL)
+            {
+                l1=l1->next;
 
-            
-            if (l1 != NULL)
-                l1 = l1->next;
-
-            if (l2 != NULL)
-                l2 = l2->next;
+            }
+            if(l2!=NULL)
+            {
+                l2=l2->next;
+            }
         }
-
-        
-        if (carry != 0) {
-            ListNode* new_node = new ListNode(carry);
-            tail->next = new_node;
-            tail = new_node;
+        if(carry!=0)
+        {
+            ListNode* new_node= new ListNode(carry);
+            tail->next=new_node;
+            tail=new_node;
         }
-
+       
         return head;
+        
     }
 };

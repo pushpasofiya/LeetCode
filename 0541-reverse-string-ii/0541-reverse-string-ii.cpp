63bef3bb02e319515efbe3b316680f6330f6ebf6
Pushpa Sofiya22 <pushpasofiyaixa@gmail.com>
2026-09-03 23:28:16 +0530

    Time: 0 ms (100%), Space: 9.5 MB (45.2%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int len=s.length();
+        for(int i=0;i<len;i+=2*k)
+        {
+        reverse(s.begin()+i,s.begin()+min(len,i+k));
+        }
+        return s;
+        
+    }
+};

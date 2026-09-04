19d3aadcf0a80536750ade499b9894e533df2f95
Pushpa Sofiya22 <pushpasofiyaixa@gmail.com>
2026-09-04 21:25:47 +0530

    Time: 0 ms (100%), Space: 9.5 MB (44.91%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.cpp
+++ 0541-reverse-string-ii/0541-reverse-string-ii.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     string reverseStr(string s, int k) {
         int len=s.length();
-        for(int i=0;i<len;i+=2*k)
+
+        for(int i=0;i<s.length();i+=2*k)
         {
-        reverse(s.begin()+i,s.begin()+min(len,i+k));
+            reverse(s.begin()+i,s.begin()+min(len,i+k));
         }
         return s;
-        
     }
 };

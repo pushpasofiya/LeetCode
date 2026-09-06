85dc3ad149b88b4b0ee5b804cb77526bb1848986
Pushpa Sofiya22 <pushpasofiyaixa@gmail.com>
2026-09-06 11:01:47 +0530

    Time: 0 ms (100%), Space: 12.5 MB (19.31%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -4,28 +4,20 @@ public:
         int i=m-1;
         int j=n-1;
         int k=m+n-1;
-        while((i>=0)&&(j>=0))
+        while(i>=0 && j>=0)
         {
-            if(nums1[i]>=nums2[j])
+            if(nums1[i]>nums2[j])
             {
-                nums1[k]=nums1[i];
-                k--;
-                i--;
+                nums1[k--]=nums1[i--];
             }
-            else if(nums2[j]>=nums1[i])
+            else
             {
-                nums1[k]=nums2[j];
-                k--;
-                j--;
+                nums1[k--]=nums2[j--];
             }
         }
-        if(j!=-1)
-        {
         while(j>=0)
         {
-            nums1[k--]=nums2[j];
-            j--;
-        }  
-        }    
+            nums1[k--]=nums2[j--];
+        }
     }
 };

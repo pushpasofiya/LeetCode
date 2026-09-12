97308f3898ff028b0dda8ea000dc39fe23459fd8
Pushpa Sofiya22 <pushpasofiyaixa@gmail.com>
2026-09-12 08:02:18 +0530

    Time: 22 ms (93.56%), Space: 82.9 MB (80.27%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        for(int l=0;l+1<nums.size();l++)
+        for(int i=1;i<nums.size();i++)
         {
-            if(nums[l]==nums[l+1])
+            if(nums[i]==nums[i-1])
             {
                 return true;
             }
         }
-    return false;
-            }
+        return false;
+
+    }
 };
